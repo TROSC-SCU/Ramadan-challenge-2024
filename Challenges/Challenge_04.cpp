9401b719cc3df05d1611d33d81bf1e6ddf3eb5fe
TROSC تروس <128643049+TROSC-SCU@users.noreply.github.com>
2024-03-17 22:32:16 +0200

    Challenge 4

---
+++ Challenges/Challenge_04.cpp
@@ -0,0 +1,19 @@
+// this challenge is very easy :)   
+// change this code to python 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num % 2 == 0) {
+        cout << num << " is Even" << endl;
+    } else {
+        cout << num << " is Odd" << endl;
+    }
+
+    return 0;
+}
